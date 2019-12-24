fd8f19be535fab8205eeacdbf020d92815fe4c29
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2019-12-24 06:59:43 +0300

    Create main_activity.kt

---
+++ Tutorial - Multiple Activities/main_activity.kt
@@ -0,0 +1,22 @@
+package com.janfranco.kotlintutorialactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun next(@Suppress("UNUSED_PARAMETER")view : View) {
+        val intent = Intent(applicationContext, NextActivity::class.java)
+        intent.putExtra("name", editText.text.toString())
+        intent.putExtra("surname", editText2.text.toString())
+        startActivity(intent)
+    }
+}
